702d8e5b735b24bc7b957eef36173bc2184cca2f
naveen <mendenaveenyadav125@gmail.com>
2025-11-26 18:29:28 +0530

    admin-module changes

---
+++ src/app/admin/components/device-indetail/device-indetail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-indetail',
+  imports: [],
+  templateUrl: './device-indetail.html',
+  styleUrl: './device-indetail.scss',
+})
+export class DeviceIndetail {
+
+}
