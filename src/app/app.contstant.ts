dfcd4743aa63922f543e317038ae4926b3a41f4b
saimaggidi <maggidisai4@gmail.com>
2025-11-26 18:52:23 +0530

    iot

--- src/app/app.contstant.ts
+++ src/app/app.contstant.ts
@@ -1,7 +1,7 @@
 export const AppConstant = {
     ORGA: {
         name: 'Kapila Hotel',
-        address: 'Yellammagutta, Hyderabad road, Nizamabad, +91 93918 85146',
+        address: 'Yellammagutta, Nizamabad, +91 93918 85146',
         websiteUrl: 'www.hotelkapila.in',
     }
 }
